ce63254729cec74ea6899a9447828a81d56fd006
sociosofia <luizvjj@gmail.com>
2026-08-01 16:44:50 -0300

    Carregar estilos da busca triangular

--- boot.js
+++ boot.js
@@ -1,3 +1,3 @@
 await import('./publication-feed.js');
-for(const href of ['structure.css','nucleo-v1.css']){const css=document.createElement('link');css.rel='stylesheet';css.href=href;document.head.appendChild(css);}
+for(const href of ['structure.css','nucleo-v1.css','search-network-v1.css']){const css=document.createElement('link');css.rel='stylesheet';css.href=href;document.head.appendChild(css);}
 await import('./home.js');
